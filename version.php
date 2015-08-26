50d6f339ca3bcfe78ff5b9d3a7fc21f1540f4adb
Skylar Kelty <s.kelty@kent.ac.uk>
2015-08-26 09:52:46 +0100

    Bump version

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015081800;
+$plugin->version   = 2015082600;
 $plugin->component = 'local_recyclebin';
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.3 (Build: 2015081300)';
+$plugin->release = '1.4 (Build: 2015082600)';
